a6f2bb7e3107675cd52860555aa9333e43e9d7bb
DoraeMishelle <dmishellehill@gmail.com>
2026-03-18 12:04:08 -0700

    fifth commit fixture updated for all function tests

---
+++ README.md
@@ -0,0 +1,13 @@
+# Awesome Agile Automation!
+# Playwright and Pytest with Python
+
+## Table Of Contents
+- [Introduction](#introduction)
+- [Python Refresher](#python-refresher)
+
+### Introduction
+#### This project serves as my python refresher and project for playwright and pytest using python. 
+#### Follow along with me on this journey of executing automated tests with python!
+
+
+### Python Refresher
--- pytest_testing/test_demo_fixtures.py
+++ pytest_testing/test_demo_fixtures.py
@@ -1,16 +1,16 @@
 # fixture is a reusable function that can be used for each test
 import pytest
 
-#make this function as a fixture
+#make this a fixture to execute before every test function
 @pytest.fixture #optional parenthesis
 def setup():
     print("setup browser...")
 
 
-def test_my_first_test():
+def test_my_first_test(setup):
     print("this is my first test")
 
-def test_my_second_test():
+def test_my_second_test(setup):
     print("this is my second test")
 
 #-----------------------------------------------------------------------------------------
@@ -26,7 +26,7 @@ def test_my_second_test():
 # -s shows all printed outputs live in console while test runs
 # -v shows detailed execution info. pytest in verbose mode
 
-
+# to show what classes look like in pytest
 class TestClass:
     def test_one(self):
         print("this is my first test")
