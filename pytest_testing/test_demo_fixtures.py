c0d20f218634f17a09c384d62020ffb39b58015f
DoraeMishelle <dmishellehill@gmail.com>
2026-03-18 12:35:08 -0700

    sixth commit scope module in fixture and return chrome

--- pytest_testing/test_demo_fixtures.py
+++ pytest_testing/test_demo_fixtures.py
@@ -1,16 +1,24 @@
-# fixture is a reusable function that can be used for each test
 import pytest
+
+# fixture is a reusable function that can be used for each test
+
+# scope = "function" (default) fixture is called before every test function
+# scope = "module" fixture is called only once before test functions
+# scope = "class" fixture is called only once before class
+# scope = "session" fixture is called only once before session
+
 
 #make this a fixture to execute before every test function
 @pytest.fixture #optional parenthesis
-def setup():
+def setup(scope = "module"):
     print("setup browser...")
-
+    return "chrome"
 
 def test_my_first_test(setup):
     print("this is my first test")
+    print("Browser is: ",setup)
 
-def test_my_second_test(setup):
+def test_my_second_test():
     print("this is my second test")
 
 #-----------------------------------------------------------------------------------------
