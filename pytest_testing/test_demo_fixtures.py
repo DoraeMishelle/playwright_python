fddefc7dd16fc12d9d1411a95775cb4833b49ffc
DoraeMishelle <dmishellehill@gmail.com>
2026-03-18 11:24:58 -0700

    second commit pytest introduction

---
+++ pytest_testing/test_demo_fixtures.py
@@ -0,0 +1,35 @@
+# fixture is a reusable function that can be used for each test
+import pytest
+
+#make this function as a fixture
+@pytest.fixture #optional parenthesis
+def setup():
+    print("setup browser...")
+
+
+def test_my_first_test():
+    print("this is my first test")
+
+def test_my_second_test():
+    print("this is my second test")
+
+#-----------------------------------------------------------------------------------------
+# run all tests in module
+#pytest test_first_test.py
+#pytest test_first_test.py -s
+#pytest test_first_test.py -s -v
+
+#Run specific tests
+#pytest test_first_test.py::test_my_first_test() -s -v
+#pytest test_first_test.py::test_my_second_test() -s -v
+
+# -s shows all printed outputs live in console while test runs
+# -v shows detailed execution info. pytest in verbose mode
+
+
+class TestClass:
+    def test_one(self):
+        print("this is my first test")
+
+    def test_two(self):
+        print("this is my first test")
---
+++ pytest_testing/test_demo_instructions.py
@@ -0,0 +1,28 @@
+import pytest
+
+def test_my_first_test():
+    print("this is my first test")
+
+def test_my_second_test():
+    print("this is my second test")
+
+#-----------------------------------------------------------------------------------------
+# run all tests in module
+#pytest test_first_test.py
+#pytest test_first_test.py -s
+#pytest test_first_test.py -s -v
+
+#Run specific tests
+#pytest test_first_test.py::test_my_first_test() -s -v
+#pytest test_first_test.py::test_my_second_test() -s -v
+
+# -s shows all printed outputs live in console while test runs
+# -v shows detailed execution info. pytest in verbose mode
+
+
+class TestClass:
+    def test_one(self):
+        print("this is my first test")
+
+    def test_two(self):
+        print("this is my first test")
