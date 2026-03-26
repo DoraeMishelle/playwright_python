faab1c676de8128679a40c0d1e7ceaabef06801e
DoraeMishelle <dmishellehill@gmail.com>
2026-03-26 13:00:26 -0700

    11th commit mostly inheritance examples added

---
+++ python_refresher/inheritance_polymorphism.py
@@ -0,0 +1,17 @@
+# example a
+
+class A:
+    def m1(self):
+        print("this is method from class A")
+
+class B(A): # single/simple inheritance
+    def m2(self):
+        print("this is method from class B")
+
+bobj = B()
+bobj.m1()
+bobj.m2()
+
+
+# example 2
+
